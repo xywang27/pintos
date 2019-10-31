3f8dc490f8c2dc92883e3c5d5b2aa23e697337fb
wangxy6 <wangxy6@shanghaitech.edu.cn>
2019-10-31 23:10:42 +0800

    1

--- userprog/syscall.h
+++ userprog/syscall.h
@@ -1,6 +1,10 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
+#include "filesys/directory.h"
 
+void syscall_init (void);
+struct dir;
+int mc;
 void syscall_init (void);
 
 #endif /* userprog/syscall.h */
