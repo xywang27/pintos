b53155f8c573b1520dd20b0281a2d8ec8f6d1824
wangxy6 <wangxy6@shanghaitech.edu.cn>
2019-11-02 19:30:46 +0800

    1

--- threads/thread.h
+++ threads/thread.h
@@ -25,6 +25,12 @@ typedef int tid_t;
 #define PRI_DEFAULT 31                  /* Default priority. */
 #define PRI_MAX 63                      /* Highest priority. */
 
+struct openfile {
+  struct list_elem elem;
+  int fd;
+  struct file *file;
+};
+
 /* A kernel thread or user process.
 
    Each thread structure is stored in its own 4 kB page.  The
