606da83c42b4e4873464710ea3e089b5f84c1710
wangxy6 <wangxy6@shanghaitech.edu.cn>
2019-11-02 12:15:57 +0800

    1

--- userprog/exception.c
+++ userprog/exception.c
@@ -148,13 +148,6 @@ page_fault (struct intr_frame *f)
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
 
-  bool is_kernel = !user;
-  if (is_kernel){
-    f->eip = (void(*)(void)) f->eax;
-    f->eax = 0;
-    return;
-  }
-
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
      which fault_addr refers. */
