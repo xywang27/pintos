66357bbe4b87802d6ae4ad763918f7491d57c7d5
wangxy6 <wangxy6@shanghaitech.edu.cn>
2019-11-08 17:05:47 +0800

    1

--- userprog/syscall.h
+++ userprog/syscall.h
@@ -1,13 +1,10 @@
-#include "threads/synch.h"
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
-typedef int pid_t;
-#define MAX 128
-#define BUFFER_SIZE 200
-void is_valid_string (const char *string);
-void is_valid_pointer (void *pointer);
+#include "threads/synch.h"
 
 void syscall_init (void);
+
+struct lock file_lock;                      /*lock that systemcall has to protect file reading and writing*/
 
 #endif /* userprog/syscall.h */
