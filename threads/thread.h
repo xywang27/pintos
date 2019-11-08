d1b24eb648e7f58b497d9f0249afe80b404699b0
wangxy6 <wangxy6@shanghaitech.edu.cn>
2019-11-08 17:07:43 +0800

    1

--- threads/thread.h
+++ threads/thread.h
@@ -5,6 +5,8 @@
 #include <list.h>
 #include <stdint.h>
 #include <threads/synch.h>
+#include "filesys/file.h"
+#include "userprog/syscall.h"
 
 /* States in a thread's life cycle. */
 enum thread_status
