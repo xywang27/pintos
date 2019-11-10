5fcbff641ec185d8a8f0c3179cc48b1dc47e182a
wangxy6 <wangxy6@shanghaitech.edu.cn>
2019-11-10 15:43:13 +0800

    1

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -23,6 +23,35 @@ static unsigned tell (int fd);
 static void close (int fd);
 static bool is_valid_fd (int fd);
 
+/* Reads a byte at user virtual address UADDR.
+   UADDR must be below PHYS_BASE.
+   Returns the byte value if successful, -1 if a segfault
+   occurred. */
+static int get_user (const uint8_t *uaddr){
+  if(!is_user_vaddr((void *)uaddr)){
+    return -1;
+  }
+  if(pagedir_get_page(thread_current()->pagedir,uaddr)==NULL){
+    return -1;
+  }
+  int result;
+  asm ("movl $1f, %0; movzbl %1, %0; 1:"
+       : "=&a" (result) : "m" (*uaddr));
+  return result;
+}
+
+/* Writes BYTE to user address UDST.
+   UDST must be below PHYS_BASE.
+   Returns true if successful, false if a segfault occurred. */
+static bool put_user (uint8_t *udst, uint8_t byte){
+  if(!is_user_vaddr(udst))
+    return false;
+  int error_code;
+  asm ("movl $1f, %0; movb %b2, %1; 1:"
+       : "=&a" (error_code), "=m" (*udst) : "q" (byte));
+  return error_code != -1;
+}
+
 /* Check whether the string is valid. */
 void
 is_valid_string (const char *string)
