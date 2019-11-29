4d52afd94aac1df198294d85cd7234f831c78661
wangxy6 <wangxy6@shanghaitech.edu.cn>
2019-11-29 13:58:32 +0800

    1

--- vm/page.h
+++ vm/page.h
@@ -1,7 +1,3 @@
-//      page.h
-//
-//      Copyright 2011 mayli <mayli.he@gmail.com>,sneakerkg<xiaotj1990327@gmail.com>
-//
 #ifndef VM_PAGE_H
 #define VM_PAGE_H
 
@@ -10,19 +6,17 @@
 #include "vm/frame.h"
 #include "vm/swap.h"
 
-/* L: a basic page elem, more vars is needed */
+/* a new struct to realize spt */
 struct spt_elem{
-  struct thread* owner;
-  /* L: the upage this entry is descrepting */
-  uint8_t *upage;
-  /* L: we can get a file ptr directly in load_seg, the following
-   * keeps the file information */
-  struct file *fileptr;
+  struct thread* owner;                                       /*the thread who holds this spt_elem*/
+  uint8_t *upage;                                              /*the upage the spt stands for*/
+
+  struct file *file;                                           /*in order to track the file*/
   off_t ofs;
   uint32_t read_bytes;
   uint32_t zero_bytes;
-  bool writable;
-  void *kpage;
+  bool writable;                                               /*if it if writable*/
+  // void *kpage;
 
   bool needclose;
   bool needremove;
