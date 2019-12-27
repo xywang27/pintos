97acfbb89634456f0c6ed1cfd69ae9e2c66e02a3
wangxy6 <wangxy6@shanghaitech.edu.cn>
2019-12-27 22:03:07 +0800

    1

--- filesys/inode.c
+++ filesys/inode.c
@@ -31,7 +31,7 @@ struct inode_disk
     block_sector_t index1;
     block_sector_t index2;
     int level;
-    unsigned is_dir;                    /* Is a directory or not. */
+    bool is_dir;                    /* Is a directory or not. */
     off_t length;                       /* File size in bytes. */
     unsigned magic;                     /* Magic number. */
 };
